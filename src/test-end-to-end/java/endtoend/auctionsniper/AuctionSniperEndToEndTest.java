1764bbf7192f1476667cb7d3f2a1c8113253f190
Jonathan Camilo <jonathan.camilo@ifood.com.br>
2020-03-15 12:03:30 -0300

    Configuring project to run end to end test with specific profile (end-to-end)

---
+++ src/test-end-to-end/java/endtoend/auctionsniper/AuctionSniperEndToEndTest.java
@@ -0,0 +1,11 @@
+package endtoend.auctionsniper;
+
+import org.junit.jupiter.api.Test;
+
+public class AuctionSniperEndToEndTest {
+
+    @Test
+    void validateThatMyEndToEndTestIsRunning() {
+
+    }
+}
